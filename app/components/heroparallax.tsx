b199f5e30511cd1daef602103629911ad9f1c145
FazeTea <byrjvkhln.a@gmail.com>
2025-01-14 18:28:06 +0800

    final

--- app/components/heroparallax.tsx
+++ app/components/heroparallax.tsx
@@ -25,16 +25,16 @@ export const HeroParallax = ({
 
   const springConfig = { stiffness: 300, damping: 30, bounce: 100 };
 
-  const translateX = useSpring(useTransform(scrollYProgress, [0, 1], [0, 4000]), springConfig);
+  const translateX = useSpring(useTransform(scrollYProgress, [0, 1], [0, 1000]), springConfig);
   const translateXReverse = useSpring(useTransform(scrollYProgress, [0, 1], [0, -1000]), springConfig);
   const rotateX = useSpring(useTransform(scrollYProgress, [0, 0.2], [15, 0]), springConfig);
   const opacity = useSpring(useTransform(scrollYProgress, [0, 0.2], [0.2, 1]), springConfig);
   const rotateZ = useSpring(useTransform(scrollYProgress, [0, 0.2], [20, 0]), springConfig);
-  const translateY = useSpring(useTransform(scrollYProgress, [0, 0.2], [-700, 0]), springConfig);
+  const translateY = useSpring(useTransform(scrollYProgress, [0, 0.2], [-700, 300]), springConfig);
   return (
     <div
       ref={ref}
-      className="pt-20 md:pt-40 overflow-hidden antialiased relative flex flex-col bg-black [perspective:1000px] [transform-style:preserve-3d] h-screen"
+      className="sm:w-[100vh] md:w-[200vh]  py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
       <motion.div
@@ -44,19 +44,19 @@ export const HeroParallax = ({
           translateY,
           opacity,
         }}
-        className="space-y-10 px-4 sm:px-8 lg:px-16 flex-grow"
+        className=""
       >
-        <motion.div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-10">
+        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20 mb-20">
           {firstRow.map((product) => (
             <ProductCard product={product} translate={translateX} key={product.title} />
           ))}
         </motion.div>
-        <motion.div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-10">
+        <motion.div className="flex flex-row  mb-20 space-x-20 ">
           {secondRow.map((product) => (
             <ProductCard product={product} translate={translateXReverse} key={product.title} />
           ))}
         </motion.div>
-        <motion.div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20">
           {thirdRow.map((product) => (
             <ProductCard product={product} translate={translateX} key={product.title} />
           ))}
@@ -104,22 +104,26 @@ export const ProductCard = ({
 }) => {
   return (
     <motion.div
-      style={{ x: translate }}
-      whileHover={{ y: -20 }}
+      style={{
+        x: translate,
+      }}
+      whileHover={{
+        y: -20,
+      }}
       key={product.title}
-      className="group/product h-72 sm:h-96 w-full relative flex-shrink-0"
+      className="group/product h-96 w-full sm:w-[20rem] md:w-[30rem] lg:w-[35rem] relative flex-shrink-0"
     >
-      <Link href={product.link} target="_blank" className="block group-hover/product:shadow-2xl">
+      <Link href={product.link} className="block group-hover/product:shadow-2xl ">
         <Image
           src={product.thumbnail}
-          height={600}
-          width={600}
-          className="object-cover object-left-top absolute h-full w-full inset-0"
+          height="600"
+          width="600"
+          className="object-contain absolute h-full w-full inset-0"
           alt={product.title}
         />
       </Link>
       <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
-      <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white text-sm md:text-lg">
+      <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white text-sm sm:text-base md:text-lg lg:text-xl">
         {product.title}
       </h2>
     </motion.div>
