65748cd28a5959aa282ce5e4cc48569857a284ef
FazeTea <byrjvkhln.a@gmail.com>
2025-04-04 16:16:17 +0800

    lol

--- app/components/back/header.tsx
+++ app/components/back/header.tsx
@@ -35,7 +35,7 @@ export const Header = ({ setIsLoggedIn }) => {
           })}
           <div
             className=" hover:text-white cursor-pointer ms-2  border px-3 py-1"
-            onClick={() => (localStorage.removeItem("isLoggedIn"), window.location.reload())}
+            onClick={() => (localStorage.removeItem("isLoggedIn"), window && window.location.reload())}
           >
             Гарах
           </div>
