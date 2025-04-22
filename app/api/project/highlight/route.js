6f13a3133a5a34d32bfffd7c58ec7605e29ec8a9
funplus2022 <funplus2022@gmail.com>
2025-04-22 16:27:51 +0800

    илүүцийг comment хийсэн!

--- app/api/project/highlight/route.js
+++ app/api/project/highlight/route.js
@@ -2,32 +2,32 @@ import dbConnect from "../../../lib/projectmodel";
 import ProjectModel from "../../../lib/projectmodel";
 import { NextResponse } from "next/server";
 
-const escpos = require('escpos');
-escpos.Network = require('escpos-network');
+// const escpos = require('escpos');
+// escpos.Network = require('escpos-network');
 
 export async function GET() {
   await dbConnect();
   try {
-    const device  = new escpos.Network('192.168.1.182', 9100);
-    // const device  = new escpos.Network('66.181.177.160', 9100);
-    const printer = new escpos.Printer(device);
+    // const device  = new escpos.Network('192.168.1.182', 9100);
+    // // const device  = new escpos.Network('66.181.177.160', 9100);
+    // const printer = new escpos.Printer(device);
 
-    device.open(function(error){
-        if (error) {
-            return console.error('Connection Error:', error);
-        }
+    // device.open(function(error){
+    //     if (error) {
+    //         return console.error('Connection Error:', error);
+    //     }
 
-        printer
-            .align('ct')                // Center
-            .style('B')                 // Bold text
-            .size(2, 2)                 // Double Width & Height
-            .text('HELLO WORLD')        // Big Text
-            .size(1, 1)                 // Normal Text
-            .text('Printing with Image') // Small Text
-            .feed(5)                    // 🔥 Add 5 empty lines after image
-            .cut()
-            .close();
-    });
+    //     printer
+    //         .align('ct')                // Center
+    //         .style('B')                 // Bold text
+    //         .size(2, 2)                 // Double Width & Height
+    //         .text('HELLO WORLD')        // Big Text
+    //         .size(1, 1)                 // Normal Text
+    //         .text('Printing with Image') // Small Text
+    //         .feed(5)                    // 🔥 Add 5 empty lines after image
+    //         .cut()
+    //         .close();
+    // });
 
     // Query to find documents where Highlight is true, sorted by `createdAt` in descending order
     const data = await ProjectModel.find({ Highlight: true }).select("title link thumbnail").sort({ createdAt: -1 }); // Sort by createdAt descending
